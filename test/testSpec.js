c99ab344823a242dc3a992c310663841a0ca7e16
rogerwillis <roger@rogerwillis.com>
2014-09-30 21:04:02 -0700

    Added UNit Testing
    Added Jasmine and Karma for unit testing

---
+++ test/testSpec.js
@@ -0,0 +1,9 @@
+﻿describe("displaymsg", function () {
+
+    it("Should display a message when btn is clicked and input...", function () {
+
+        $(".btn-submit").on('click');
+        expect($('.results')).toHaveText('Wrong, something is!!! Put some text in sir!!! Hmmmmmm.');
+
+    });
+});
